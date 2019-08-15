25a8f01d6620b8d9fcea70db025ef414de445bd7
Lucas Henrique <lucas@superare.co>
2019-08-15 16:38:43 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# carpano.js
+# Carpano.js
 Just a joke for my closest friends &lt;3
+
+## Installing
+There's just a single type of installation (npm global package). Just run the command `npm i -g carpano.js`.
+
+## Options
+| Command | Description |
+| --help  | Default: "Meu caralho enrolado num pano" |
---
+++ bin/index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const open = require('open');
+const notifier = require('node-notifier');
+const path = require('path');
+const inquirer = require('inquirer');
+const args = process.argv.slice(2);
+const os = require('os');
+
+/**
+ * "E as boas práticas?"
+ * Pega o carpano e aproveita
+ */
+
+if ( args.length === 0 ) {
+  let qt = {
+    type: "list",
+    name: "qt",
+    message: "Vc sabe o que eh carpano?",
+    choices: [
+      "sim",
+      "nao"
+    ]
+  }
+
+  return inquirer.prompt(qt).then(res => {
+    if (res.qt === 'nao') return console.log('Eh meu caralho enrolado num pano');
+    return console.log('😏');
+  })
+}
+
+args.map((arg, index) => {
+  if ( arg === '--help' ) return console.log("Meu caralho enrolado num pano - v0.0.1");
+  if ( arg === '--tutorial' ) return open('https://www.youtube.com/watch?v=eeBP1wQzsm0');
+  
+  if ( arg === '--notify' ) {
+    notifier.notify({
+      title: `${os.userInfo().username.toUpperCase()}`,
+      message: "Vc sabe o que eh carpano?",
+      icon: path.join(__dirname, '../icons/icon.png'),
+      sound: true
+    })
+
+    setTimeout(function() {
+      notifier.notify({
+        title: ``,
+        message: "Eh meu caralho enrolado num pano!",
+        icon: path.join(__dirname, '../icons/icon.png'),
+        sound: true
+      })
+    }, 5000);
+  }
+});
